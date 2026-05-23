38e892b5c1bdb7b03afd266c748188aeef15892c
Piero <pier0ocj77@gmail.com>
2026-05-23 10:35:31 -0500

    feat: implementar clases Product y Cart para gestión del carrito de compras, incluyendo el registro de los productos, asi como tambien la agregacion de los carritos y el calculo total a pagar por estos

--- virtual-store/src/model/Cart.java
+++ virtual-store/src/model/Cart.java
@@ -1,4 +1,21 @@
 package model;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Cart {
+    private List<Product> products = new ArrayList<>();
+    public void addProduct(Product product){
+        products.add(product);
+        System.out.println("Producto agregado " + product);
+    }
+    public double getTotal(){
+        double total=0;
+        for (Product p : products){
+            total +=p.getPrice();
+        }
+        return total;
+    }
+    public List<Product> getProducts(){
+        return products;
+    }
 }
+
